5f7c71df2e0724d287a961eda16d3fa83cc99fd9
santiagonieto09 <santiago.nieto09@gmail.com>
2025-03-04 20:52:48 -0500

    Proyecto Api-Rest Gestion de Formatos

---
+++ src/main/java/co/edu/unicauca/fiet/asae/core/gestion_formatos_api_rest/GestionFormatosApiRestApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.fiet.asae.core.gestion_formatos_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFormatosApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFormatosApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unicauca/fiet/asae/core/gestion_formatos_api_rest/GestionFormatosApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.fiet.asae.core.gestion_formatos_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFormatosApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
